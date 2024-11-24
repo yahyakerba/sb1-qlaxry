de8f0bf24a44b971b92368e00ae85e7984b0bdef
Yahya Kerba <7980470+yahyakerba@users.noreply.github.com>
2024-11-24 03:08:15 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +10,11 @@ export default defineConfig({
   server: {
     headers: {
       'Service-Worker-Allowed': '/'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
